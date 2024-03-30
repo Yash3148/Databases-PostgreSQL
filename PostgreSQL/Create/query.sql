af5e9adf48b5965064716e8edde24a7b6f7dc2d4
Yash <yashsingh3148@gmail.com>
2024-03-30 23:06:23 +0530

    DataBases

---
+++ PostgreSQL/Create/query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE capitals (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(45),
+    capital VARCHAR(45)
+)
+
+CREATE TABLE capitals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(45),
+    flag TEXT
+)
